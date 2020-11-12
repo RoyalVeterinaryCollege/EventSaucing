ff711d984f1434a170eb2c6929b34b20353ae4bc
Noel <nkennedy@rvc.ac.uk>
2020-11-12 10:48:05 +0000

    working on a base reactor for convenience

--- Reactors/ConventionalReactionDispatcher.cs
+++ Reactors/ConventionalReactionDispatcher.cs
@@ -1,40 +1,32 @@
-﻿using Dapper;
-using EventSaucing.NEventStore;
-using NEventStore;
-using Serilog;
+﻿using NEventStore;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
+namespace EventSaucing.Reactors {
 
-
-namespace EventSaucing.Reactors
-{
-
-    public delegate void ProjectionMethod(IDbTransaction tx, ICommit commit, object @event);
 
     /// <summary>
-    /// A delegate describing the signature of a method which projects an event
+    /// A delegate describing the signature of a method which projects an event or article
     /// </summary>
-    public delegate Task ConventionalReactionMethod(object reactor, object @event);
+    public delegate Task ConventionalReactionMethod(object reactor, object payload);
 
     /// <summary>
     /// An event dispatcher for reactors which react to aggregate events
     /// 
     /// Looks for methods which start with 'Apply' and have one parameter.  These methods can be either async or sync
     /// </summary>
-    public class ConventionalReactorAggregateEventDispatcher
+    public class ConventionalReactionDispatcher
     {
         /// <summary>
-        /// Typeof event -> to method which can project that event
+        /// Typeof event -> to method which can react to that event/article
         /// </summary>
         Dictionary<Type, ConventionalReactionMethod> _dispatchTable;
         private readonly Type reactorType;
 
-        public ConventionalReactorAggregateEventDispatcher(Type reactorType)  {
+        public ConventionalReactionDispatcher(Type reactorType)  {
             this.reactorType = reactorType;
 
             BuildDispatchTable();
@@ -45,7 +37,7 @@ namespace EventSaucing.Reactors
         /// </summary>
         /// <param name="method"></param>
         /// <returns></returns>
-        private bool IsAggregateReactionMethod(MethodInfo method)  {
+        private bool IsReactionMethod(MethodInfo method)  {
             if (!method.Name.StartsWith("Apply")) return false;
             var parameters = method.GetParameters();
             if (parameters.Length != 1) return false;
@@ -56,13 +48,13 @@ namespace EventSaucing.Reactors
             if(method.ReturnType == typeof(Task)) {
                 //async
                 return new ConventionalReactionMethod(
-                    (reactor, @event) => {return (Task)method.Invoke(reactor, new[] { @event });
+                    (reactor, payload) => {return (Task)method.Invoke(reactor, new[] { payload });
                 });
             } else {
                 //wrap sync method
                 return new ConventionalReactionMethod(
-                    (reactor, @event) => {
-                       var result= method.Invoke(reactor, new[] { @event });
+                    (reactor, payload) => {
+                       var result=method.Invoke(reactor, new[] { payload });
                        return Task.CompletedTask;
                     }
                 );
@@ -71,36 +63,31 @@ namespace EventSaucing.Reactors
 
         public void BuildDispatchTable()  {
             _dispatchTable = (from method in reactorType.GetMethods()
-                              where IsAggregateReactionMethod(method)
+                              where IsReactionMethod(method)
                               select new
                               {
-                                  EventType = method.GetParameters()[0].ParameterType // only one parameter and thats the event
+                                  EventType = method.GetParameters()[0].ParameterType // only one parameter and thats the payload : either an event or an article
                                   ,
                                   InvocationMethod = MakeAsyncInvocation(method)
                               })
                          .ToDictionary(x => x.EventType, x => x.InvocationMethod);
         }
-        public async Task ReactAsync(object reactor, IEventStream stream) {
+        public async Task DispatchEventStreamAsync(IReactor reactor, IEventStream stream) {
             foreach (EventMessage eventMessage in stream.CommittedEvents) {
-                await ReactAsync(reactor, eventMessage.Body);
+                await DispatchPayloadAsync(reactor, eventMessage.Body);
             }
         }
-        public async Task ReactAsync(object reactor, object @event)  {
-            Type eventType = @event.GetType();
+        /// <summary>
+        /// Dispatches the payload to the reactor
+        /// </summary>
+        /// <param name="reactor">IReactor</param>
+        /// <param name="payload">object Either the event or the article</param>
+        /// <returns></returns>
+        public async Task DispatchPayloadAsync(IReactor reactor, object payload)  {
+            Type eventType = payload.GetType();
             if (!_dispatchTable.ContainsKey(eventType)) return ;
             var projectionMethod = _dispatchTable[eventType];
-            await projectionMethod(reactor, @event);
-        }
-        public async Task ReactAsync(object reactor, ICommit commit)  {
-            foreach (var eventMessage in commit.Events)  {
-                try {
-                    await ReactAsync(reactor, eventMessage);
-                }  catch (Exception)  {
-                    //todo error handling here or nah?
-                    //logger.Error(error.InnerException, $"{nameof(ConventionalReactorAggregateEventDispatcher)} caught exception when trying to project event {eventType} in commit {commit.CommitId} at checkpoint {commit.CheckpointToken} for aggregate {commit.AggregateId()}");
-                    throw;
-                }
-            }
+            await projectionMethod(reactor, payload);
         }
     }
 }
---
+++ Reactors/ReactorExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using EventSaucing.Reactors.Messages;
+using NEventStore;
+using System;
+using System.Linq;
+
+namespace EventSaucing.Reactors {
+    public static class ReactorExtensionMethods {
+
+        /// <summary>
+        /// Loads the events from the eventstream which have not yet been dispatched to the Reactor
+        /// </summary>
+        /// <param name="storeEvents"></param>
+        /// <param name="uow"></param>
+        /// <param name="msg"></param>
+        /// <returns>IEventStream the (partial) stream of events which have not yet been dispatched to the Reactor</returns>
+        public static IEventStream LoadUndispatchedEvents(this IStoreEvents storeEvents, IUnitOfWork uow, SubscribedAggregateChanged msg) {
+            int fromStreamRevision = 
+                uow.PersistedPubSub
+                   .Map(previous => previous.AggregateSubscriptions.First(x => x.AggregateId == msg.AggregateId).StreamRevision + 1)
+                   .GetOrElse(1);
+            return LoadUndispatchedEvents(storeEvents, msg.AggregateId, fromStreamRevision);
+        }
+        /// <summary>
+        /// Loads the events from the eventstream optionally after a particular StreamRevision
+        /// </summary>
+        /// <param name="storeEvents"></param>
+        /// <returns>IEventStream the (potentially partial) stream of events which have not yet been dispatched to the Reactor</returns>
+        public static IEventStream LoadUndispatchedEvents(this IStoreEvents storeEvents, Guid aggregateId, int streamRevision = 1) {
+            return storeEvents.OpenStream(aggregateId, streamRevision);
+        }
+    }
+}
