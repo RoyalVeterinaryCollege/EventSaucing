2c67cd1793bb8b6368767e7bf43f04ebb6c99bf5
Noel <nkennedy@rvc.ac.uk>
2021-07-14 11:20:16 +0100

    upgraded dependencies

--- DependencyInjection/Autofac/AkkaModule.cs
+++ DependencyInjection/Autofac/AkkaModule.cs
@@ -32,7 +32,7 @@ namespace EventSaucing.DependencyInjection.Autofac {
             //should come first so akka is configured to use autofac as early as possible during start up, else various components will fail as they can't find their dependencies
             builder.RegisterType<AutoFacDependencyResolver>().As<IDependencyResolver>().SingleInstance();
             builder.RegisterType<AkkaAutofacConfigurer>().As<IStartable>();
-            builder.RegisterType<Akka.AkkaShutdown>().As<IStartable>();
+            builder.RegisterType<Akka.AkkaStartStop>().As<IStartable>();
 
 			
 			builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly()).AssignableTo<ProjectorBase>(); // Get the assembly that kicks the show off, this should have projectors in it.
@@ -41,7 +41,7 @@ namespace EventSaucing.DependencyInjection.Autofac {
 
             builder
                 .Register(x => ActorSystem.Create(actorsystemname, config))
-                .SingleInstance(); // Akka starts at this point
+                .SingleInstance(); // Akka starts at this point, but without the dependency resolver
         }
     }
 }
