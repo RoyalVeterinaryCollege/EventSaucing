8206b3bdc63a34ef227af6ed3a2c7af58764933e
Noel <nkennedy@rvc.ac.uk>
2021-10-06 09:40:05 +0100

    added type provider which registers the typical projectors you want to use

---
+++ Projectors/DefaultProjectorTypeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSaucing.Reactors;
+
+namespace EventSaucing.Projectors {
+    /// <summary>
+    /// An implementation of IProjectorTypeProvider which adds <see cref="ReactorAggregateSubscriptionProjector"></see> and all types added by <see cref="EntryAssemblyProjectorTypeProvider"></see>
+    /// </summary>
+    public class DefaultProjectorTypeProvider : IProjectorTypeProvider {
+        public IEnumerable<Type> GetProjectorTypes() {
+            var types = new List<Type> {typeof(ReactorAggregateSubscriptionProjector)};
+            types.AddRange(new EntryAssemblyProjectorTypeProvider().GetProjectorTypes());
+            return types;
+        }
+    }
+}
