4d112e9c42d69db6a54a0fa7a6711a24d01e892a
Noel <nkennedy@rvc.ac.uk>
2022-02-04 09:46:06 +0000

    More tests

--- EventSaucing.Tests/EventStream/InMemoryCommitSerialiserCacheTests.cs
+++ EventSaucing.Tests/EventStream/InMemoryCommitSerialiserCacheTests.cs
@@ -40,10 +40,9 @@ namespace EventSaucing.EventStream
             sut.GetCommitsAfter(9L)
                 .Should().ContainSingle(x => x.Commit == _fakeCommit1);
         }
-
     }
 
-    public class When_cache_contains_two_commits : InMemoryCommitSerialiserCacheTests
+    public class When_cache_contains_two_contiguous_commits : InMemoryCommitSerialiserCacheTests
     {
         private FakeCommit _fakeCommit1;
         private FakeCommit _fakeCommit2;
@@ -56,22 +55,54 @@ namespace EventSaucing.EventStream
             //note out of order
             sut.Cache(_fakeCommit2);
             sut.Cache(_fakeCommit1);
-
         }
         
         [Test]
-        public void It_returns_expected_count_of_commits()
-        {
+        public void It_returns_expected_count_of_commits() {
             sut.GetCommitsAfter(9L)
                 .Should().HaveCount(2);
         }
 
         [Test]
-        public void It_returns_commits_in_correct_order()
-        {
+        public void It_returns_commits_in_correct_order()  {
             sut.GetCommitsAfter(9L)
                 .Select(x => x.Commit.CheckpointToken)
                 .Should().BeEquivalentTo(new[] { 10L, 11L });
+        }
+    }
+
+    public class When_cache_contains_two_non_contiguous_commits : InMemoryCommitSerialiserCacheTests
+    {
+        private FakeCommit _fakeCommit1;
+        private FakeCommit _fakeCommit2;
+
+        protected override void Because()
+        {
+            base.Because();
+            _fakeCommit1 = new FakeCommit() { CheckpointToken = 10L };
+            _fakeCommit2 = new FakeCommit() { CheckpointToken = 12L };//note gap
+            sut.Cache(_fakeCommit1);
+            sut.Cache(_fakeCommit2);
+        }
+
+        [Test]
+        public void It_returns_only_the_commit_it_can_place_in_order()
+        {
+            sut.GetCommitsAfter(9L)
+                .Should().HaveCount(1);
+        }
+
+        [Test]
+        public void It_returns_only_the_correct_commit() {
+            sut.GetCommitsAfter(9L)
+                .Should().ContainSingle(x => x.Commit == _fakeCommit1);
+        }
+
+        [Test]
+        public void It_returns_the_later_commit_if_requested()
+        {
+            sut.GetCommitsAfter(11L)
+                .Should().ContainSingle(x => x.Commit == _fakeCommit2);
         }
     }
 }
