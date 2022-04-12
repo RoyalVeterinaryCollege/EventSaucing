cba956a8356ad16c4e0e69f106660bbb9948d768
Noel <nkennedy@rvc.ac.uk>
2022-04-12 16:17:08 +0100

    flushing out an example stream processor

---
+++ ExampleApp.Domain/Order.cs
@@ -0,0 +1,19 @@
+﻿using EventSaucing.Aggregates;
+using ExampleApp.Events;
+using Scalesque;
+
+namespace ExampleApp.Domain;
+
+public class Order : Aggregate
+{
+    public Order(Guid id) {
+        Id = id;
+    }
+
+    readonly Dictionary<string, int> items = new Dictionary<string, int>();
+    public void AddItem(string itemName, int quantity) {
+        RaiseEvent(new ItemOrdered(itemName, quantity));
+    }
+
+    void Apply(ItemOrdered @evt) => items[@evt.name] = items.GetOrElse(evt.name,()=>0) + @evt.quantity;
+}
---
+++ ExampleApp/OrderCounting/OrderCountingStreamProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using EventSaucing.Storage;
+using EventSaucing.StreamProcessors;
+using EventSaucing.StreamProcessors.Projectors;
+using NEventStore;
+using NEventStore.Persistence;
+
+namespace ExampleApp.OrderCounting
+{
+    public class OrderCountingStreamProcessor : SqlProjector {
+        private readonly IDbService _dbService;
+
+        public OrderCountingStreamProcessor(IDbService dbService, IPersistStreams persistStreams,
+            SqlProjectorCheckPointPersister persister) : base(persistStreams, persister) {
+            _dbService = dbService;
+        }
+
+        public override DbConnection GetProjectionDb() => _dbService.GetReplica();
+
+    }
+}
