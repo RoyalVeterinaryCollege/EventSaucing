c02446239f105039206fb1ebd3571a01ad942061
Noel <nkennedy@rvc.ac.uk>
2022-04-20 15:55:23 +0100

    Distributed stream processing is working!!!

--- ExampleApp/Services/UserActivitySimulatorService.cs
+++ ExampleApp/Services/UserActivitySimulatorService.cs
@@ -1,4 +1,5 @@
-﻿using ExampleApp.Domain;
+﻿using System.Net;
+using ExampleApp.Domain;
 using NEventStore.Domain.Persistence;
 
 namespace ExampleApp.Services
@@ -18,8 +19,7 @@ namespace ExampleApp.Services
             stoppingToken.Register(() =>
                 _logger.LogDebug($" GracePeriod background task is stopping."));
 
-            while (!stoppingToken.IsCancellationRequested)
-            {
+            while (!stoppingToken.IsCancellationRequested)  {
                 _logger.LogDebug($"GracePeriod task doing background work.");
 
                 await SimulateSomeActivityAsync();
@@ -31,10 +31,25 @@ namespace ExampleApp.Services
         }
 
         private Task SimulateSomeActivityAsync() {
-            var order = new Order(Guid.NewGuid());
-            var items = new List<string>() { "Book", "Film", "Pen" };
-            order.AddItem(items[_rnd.Next(items.Count)],1);
-            _repository.Save(order, Guid.NewGuid());
+            try {
+                var orders = new List<Guid>() {
+                    Guid.Parse("c26b5d00-6dc5-43f5-849c-7cd21dbdf127"),
+                    Guid.Parse("24003666-8d84-4e3f-856e-89987dde499d"),
+                    Guid.Parse("41bb49a4-806b-4c44-a7ca-6d3300140b86")
+                };
+                var items = new List<string>() { "Book", "Film", "Pen" };
+
+                var order = _repository.GetById<Order>(orders[_rnd.Next(orders.Count)]);
+                if (order is null) order = new Order(orders[_rnd.Next(orders.Count)]);
+                order.AddItem(items[_rnd.Next(items.Count)], 1);
+                _repository.Save(order, Guid.NewGuid(), headers => headers["node"] = Dns.GetHostName());
+            }
+            catch (ConflictingCommandException) {
+                // just hide this.  both nodes happened to save same order at same time
+                // in real app, one user would lose their changes and would have to retry
+            }
+
+
             return Task.CompletedTask;
         }
     }
