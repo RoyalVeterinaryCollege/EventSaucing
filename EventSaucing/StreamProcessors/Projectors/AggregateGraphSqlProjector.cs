2df99916cc51cbfe5890076d6521875ec4d98b47
Noel <nkennedy@rvc.ac.uk>
2022-05-11 12:06:11 +0100

    added new columns and fixed bug on hashing

--- EventSaucing/StreamProcessors/Projectors/AggregateGraphSqlProjector.cs
+++ EventSaucing/StreamProcessors/Projectors/AggregateGraphSqlProjector.cs
@@ -22,6 +22,7 @@ namespace EventSaucing.StreamProcessors.Projectors
         }
 
         public override async Task<bool> ProcessAsync(ICommit commit) {
+            /* commented out to speed up catch up
             // make this idempotent
             using (var con = GetProjectionDb()) {
                 await con.OpenAsync();
@@ -29,7 +30,7 @@ namespace EventSaucing.StreamProcessors.Projectors
                 var results = await con.QueryAsync<int>("SELECT TOP 1 1 FROM dbo.AggregateGraph WHERE CheckpointNumber = @CheckpointToken",
                     new { commit.CheckpointToken });
                 if (results.Any()) return true;
-            }
+            }*/
 
             var hasher = new Sha1StreamIdHasher();
 
@@ -48,10 +49,11 @@ namespace EventSaucing.StreamProcessors.Projectors
                                 var targetGuid = (Guid)propertyInfo.GetValue(evt);
 
                                 var results = con.Query<int>(
-                                    "SELECT TOP 1 1 FROM dbo.Commits WHERE BucketId=@BucketId AND StreamId = @StreamId",
+                                    "SELECT TOP 1 1 FROM dbo.Commits WHERE BucketId=@BucketId AND StreamId = @StreamId AND StreamIdOriginal = @AggregateId",
                                     new { 
-                                        BucketId = new DbString { Value = commit.BucketId, IsFixedLength = false, Length = 40, IsAnsi = true},
-                                        StreamId = new DbString { Value = hasher.GetHash(targetGuid.ToString()), IsFixedLength = true, Length = 40, IsAnsi = true }
+                                        BucketId = new DbString { Value = commit.BucketId, IsFixedLength = false, Length = 40, IsAnsi = true}, //nb IsAnsi seems to toggle to nvarchar/nchar vs varchar/nvarchar
+                                        StreamId = new DbString { Value = hasher.GetHash(targetGuid.ToString()), IsFixedLength = true, Length = 40, IsAnsi = true },
+                                        AggregateId = targetGuid.ToString() //this confirms existence as StreamId is a hash 
                                     }
                                 );
                                 return results.Any();
@@ -66,26 +68,32 @@ namespace EventSaucing.StreamProcessors.Projectors
 
             var sb = new StringBuilder(@"
 INSERT INTO [dbo].[AggregateGraph]
-    ([SourceId]
-    ,[TargetId]
-    ,[EventName]
-    ,[Label]
-    ,[CheckpointNumber])
-VALUES
-");
+	([SourceId]
+	,[SourceStreamId]
+	,[SourceAggregateType]
+	,[EventName]
+	,[Label]
+	,[TargetId]
+	,[TargetStreamId]
+	,[CheckpointNumber])
+VALUES ");
 
             var first = true;
             foreach (var eventAggregateProperties in aggregateProperties) {
                 foreach (var propertyInfo in eventAggregateProperties.Properties) {
                     var sourceId = commit.AggregateId();
-                    var targetId = (Guid)propertyInfo.GetValue(eventAggregateProperties.Event);
+                    var sourceStreamId = hasher.GetHash(commit.AggregateId().ToString());
+                    var sourceAggregateType = (string)commit.Headers["AggregateType"];
                     var eventName = eventAggregateProperties.Event.GetType().Name;
                     var label = propertyInfo.Name.Length < 500 ? propertyInfo.Name : propertyInfo.Name.Substring(0,500);
+                    var targetId = (Guid)propertyInfo.GetValue(eventAggregateProperties.Event);
+                    var targetStreamId = hasher.GetHash(targetId.ToString());
                     var checkpointNumber = commit.CheckpointToken;
-                    sb.AppendLine($"{(!first ? "," : "")}('{sourceId}', '{targetId}', '{eventName}', '{label}', {checkpointNumber})");
+                    sb.AppendLine($"{(!first ? "," : "")}('{sourceId}','{sourceStreamId}','{sourceAggregateType}', '{eventName}', '{label}','{targetId}', '{targetStreamId}', {checkpointNumber})");
                     first = false;
                 }
             }
+
             using (var con = GetProjectionDb()) {
                 await con.OpenAsync();
                 await con.ExecuteAsync(sb.ToString());
